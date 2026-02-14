bbf4b254b508de5ac50b79aaa745cfcf990d001a
kylarhm03 <kaylaramadhani312@gmail.com>
2026-02-14 10:24:51 +0700

    to do list organigram data

--- core/entity/cabinet_info_entity.go
+++ core/entity/cabinet_info_entity.go
@@ -4,12 +4,14 @@ import "github.com/google/uuid"
 
 type CabinetInfo struct {
 	Timestamp
-	Id       uuid.UUID  `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()" json:"id"`
-	Visi     string     `gorm:"type:text;not null" json:"visi"`
-	Misi     string     `gorm:"type:text;not null" json:"misi"`
-	Tagline  string     `gorm:"type:varchar(255)" json:"tagline"`
-	Period   string     `gorm:"type:varchar(10);not null" json:"period"`
-	LogoId   *uuid.UUID `gorm:"type:uuid" json:"logo_id"`
-	Logo     *Gallery   `gorm:"foreignKey:LogoId" json:"logo"`
-	IsActive bool       `gorm:"default:true" json:"is_active"`
+	Id       		uuid.UUID  `gorm:"type:uuid;primaryKey;default:uuid_generate_v4()" json:"id"`
+	Visi     		string     `gorm:"type:text;not null" json:"visi"`
+	Misi     		string     `gorm:"type:text;not null" json:"misi"`
+	Tagline  		string     `gorm:"type:varchar(255)" json:"tagline"`
+	Period   		string     `gorm:"type:varchar(10);not null" json:"period"`
+	LogoId   		*uuid.UUID `gorm:"type:uuid" json:"logo_id"`
+	Logo     		*Gallery   `gorm:"foreignKey:LogoId" json:"logo"`
+	OrganigramId    *uuid.UUID `gorm:"type:uuid" json:"organigram_id"`
+	Organigram      *Gallery   `gorm:"foreignKey:OrganigramId" json:"organigram"`
+	IsActive 		bool       `gorm:"default:true" json:"is_active"`
 }
