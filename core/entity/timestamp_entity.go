6fd645837b55f509277f6f17e9b36013fead4d68
Fastering18 <brahmana165@gmail.com>
2026-03-02 15:55:28 +0700

    feat: hard delete and jwt expiration env

--- core/entity/timestamp_entity.go
+++ core/entity/timestamp_entity.go
@@ -2,12 +2,9 @@ package entity
 
 import (
 	"time"
-
-	"gorm.io/gorm"
 )
 
 type Timestamp struct {
 	CreatedAt time.Time `gorm:"type:timestamp without time zone" json:"created_at"`
 	UpdatedAt time.Time `gorm:"type:timestamp without time zone" json:"updated_at"`
-	DeletedAt gorm.DeletedAt
 }
